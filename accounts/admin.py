5c5c9e49c409ff1925fb8f0379257b877ae569f9
Mohamed Fawzy <m0hamed.fawzy.1062002@gmail.com>
2026-06-24 00:24:48 +0300

    reviews

--- accounts/admin.py
+++ accounts/admin.py
@@ -18,3 +18,12 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+# from django.contrib import admin
+# from django.contrib.auth import get_user_model
+#
+# CustomUser = get_user_model()
+#
+#
+# @admin.register(CustomUser)
+# class CustomUserAdmin(admin.ModelAdmin):
+#     list_display = ["email", "username", "is_superuser"]
