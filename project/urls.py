67bfcd8cca8802ef3a0c5a9700fd45ee6a0cfbe2
Mohamed Fawzy <m0hamed.fawzy.1062002@gmail.com>
2026-06-30 23:02:56 +0300

    user_upload_media_files

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # django admin
@@ -10,4 +12,6 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path('', include('pages.urls')),
     path("books/", include("books.urls")),
-]
+] + static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
